a5866d82311982c64b6bd8af585de25530260b18
acchmedha <ahmedha@maestralsolutions.com>
2021-08-31 18:00:16 +0200

    users-controller-edited

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -72,7 +72,6 @@ namespace JAP_Task_1_MoviesApi.Controllers
         }
 
         // POST: api/Users
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<User>> PostUser(User user)
         {
